baaf377c208817e598cbd5bfa48c91ddf5c42a5e
Mica78 <mica78@yandex.ru>
2023-11-27 15:34:29 +0400

    task2 init

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,35 @@
+const clicker = document.getElementById("cookie")
+const clickerCounter =  document.getElementById("clicker__counter");
+const clickerAvgCounter = document.getElementById("clicker__average")
+
+let clickCounter = parseInt(clickerCounter.textContent);
+
+let isLittleImage = true;
+
+let isStart = true;
+let start =  Date.now()
+
+
+clicker.onclick = () => {
+    clickCounter += 1;
+    clickerCounter.textContent = clickCounter;
+
+    if (isLittleImage) {
+        clicker.width =  clicker.width * 2;
+        isLittleImage = false;
+    } else {
+        clicker.width = clicker.width / 2;
+        isLittleImage = true;
+    }
+    let finish = Date.now();
+
+    if (isStart) {
+        clickerAvgCounter.textContent = (1 / ((finish - start) / 1000)).toFixed(2);
+        start = Date.now();
+        isStart = false;
+    } else {
+        clickerAvgCounter.textContent = (1 / ((finish - start) / 1000)).toFixed(2);;
+        start = Date.now();
+        isStart = true;
+    }
+}
