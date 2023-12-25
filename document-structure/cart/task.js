aeb50337c62dcef4c6bc8a87c71f8590d9804c85
Mica78 <mica78@yandex.ru>
2023-12-25 10:34:44 +0400

    task3 added

---
+++ document-structure/cart/task.js
@@ -0,0 +1,55 @@
+const products = document.querySelectorAll('.product');
+const cartProducts = document.querySelector('.cart__products');
+
+const addProduct = function() {
+    for (let i = 0; i < products.length; i++) {
+        products[i].addEventListener('click', (e) =>{
+            let dec = products[i].querySelector('.product__quantity-control_dec');
+            let inc = products[i].querySelector('.product__quantity-control_inc');
+            let value = products[i].querySelector('.product__quantity-value');
+            let id = products[i].dataset.id;
+            let imageSrc = products[i].querySelector('img').src;
+            const htmlExp = `<div class="cart__product" data-id="${id}">
+                            <img class="cart__product-image" src="${imageSrc}">
+                            <div class="cart__product-count">${value.textContent}</div>
+                            <button class="product__add" style="position: absolute; left: 0px; right: 0px; bottom: -15px;">Удалить</button>`
+            let isInCart = false;
+            if(e.target === inc) {
+                value.textContent = Number(value.textContent) + 1;
+            } else if(e.target === dec) {
+                value.textContent = Number(value.textContent) > 0 ? Number(value.textContent) - 1 : value.textContent;
+            };
+            if (e.target.className === 'product__add') {
+                for (let cartProduct of cartProducts.children) {
+                    if (cartProduct.dataset.id === id) {
+                        let count = cartProduct.querySelector('.cart__product-count');
+                        count.textContent = Number(count.textContent) + Number(value.textContent);
+                        isInCart = true;
+                    };
+                };
+                if (isInCart === false) {
+                    cartProducts.insertAdjacentHTML('beforeEnd', htmlExp);
+                };
+            };
+        });
+    };
+};
+
+const deleteProduct = function() {
+    let deleteBtn = document.querySelectorAll('button');
+    for (btn of deleteBtn) {
+        btn.addEventListener('click', (event) => {
+            event.target.parentNode.remove();
+        });
+    };
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    addProduct();
+});
+
+const observer = new MutationObserver(() => {
+    deleteProduct();
+});
+
+observer.observe(cartProducts, {childList: true,});
